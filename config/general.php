0b8dc1eb9abe3824a416afc1fc12ffcdf70c91c8
Burkhard Meyer <burkhard.meyer@gmail.com>
2018-04-23 18:11:45 +0200

    Upgrade auf Craft 3.0.2 & Gulp Files aktualisiert

--- config/general.php
+++ config/general.php
@@ -46,7 +46,7 @@ return [
         // Custom site-specific config settings
         'custom' => [
             'craftEnv' => CRAFT_ENVIRONMENT,
-            'staticAssetsVersion' => 27,
+            'staticAssetsVersion' => 31,
         ]
     ],
 
